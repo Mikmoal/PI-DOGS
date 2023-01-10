d5452b5657ee5268918e3bffb7d9d6574bb0a6ad
Mikmoal <miguel.moam@outlook.com>
2023-01-09 21:02:26 -0600

    limpieza

--- api/src/routes/controllers.js
+++
@@ -1,4 +0,0 @@
-// const { Sequelize } = require("sequelize");
-// const Breed = require("../models/Breed")(Sequelize);
-// const Temperament = require("../models/Temperament")(Sequelize);
-// const axios = require("axios");
